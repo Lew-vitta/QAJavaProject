53e8bbb6b242353674ef6af886cf52b3fd64db30
lew-Vitta <lewis.vitta22@gmail.com>
2021-11-24 05:49:36 +0000

    +Start creating Product table

--- src/main/java/com/vitta/SkateXut/model/Product.java
+++ src/main/java/com/vitta/SkateXut/model/Product.java
@@ -1,34 +1,45 @@
 package com.vitta.SkateXut.model;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
-
+@Entity
+@Table(name = "Product")
 public class Product{
 
+    @Column(name = "Product_name")
     private String Product;
+
+    @Column(name = "Product_Barcode", unique = true)
     private int Barcode;
+
     private String Description;
     private String Colour;
     private String Dimentions;
     private String Materials;
+    private int Stock;
 
     public Product(String product) {
         Product = product;
     }
 
     public Product(String product, int barcode) {
+        super();
         this.Product = product;
         this.Barcode = barcode;
     }
 
-    public Product(String product, int barcode, String description, String colour, String dimentions, String materials) {
+    public Product(String product, int barcode, String description, String colour, String dimentions, String materials, int stock) {
+        super();
         this.Product = product;
         this.Barcode = barcode;
         this.Description = description;
         this.Colour = colour;
         this.Dimentions = dimentions;
         this.Materials = materials;
+        this.Stock = stock;
     }
 
     public String getProduct() {
@@ -77,5 +88,13 @@ public class Product{
 
     public void setMaterials(String materials) {
         Materials = materials;
+    }
+
+    public int getStock() {
+        return Stock;
+    }
+
+    public void setStock(int stock) {
+        this.Stock = stock;
     }
 }
